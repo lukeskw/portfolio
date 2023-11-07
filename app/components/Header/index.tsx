8d7e67f8fe52c699af40ed16b9946a69d063c8aa
lukeskw <lucasporfirioa@gmail.com>
2023-11-07 04:14:20 -0300

    ft: adding localstorage to remember last language

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -2,15 +2,32 @@
 import { LanguageProps } from '@/app/@types/language'
 import ReactCountryFlag from 'react-country-flag'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
+import { useEffect } from 'react';
 
 export default function Header({ language: lang }: LanguageProps) {
+
+  useEffect(() => {
+    const localLanguage = localStorage.getItem('language') || 'en-US';
+    if (localLanguage !== lang) {
+      redirect(`/${localLanguage}`);
+    }
+  }, [lang]);
+
+  function toggleLanguage(toggledLanguage: string) {
+    if (toggledLanguage !== lang) {
+      localStorage.setItem('language', toggledLanguage);
+      redirect(`/${toggledLanguage}`);
+    }
+  }
+
   return (
     <header className="flex w-full items-end justify-end">
       <div className="flex items-center justify-center gap-4 px-0 md:px-12">
-        <Link href="/en-US">
+        <Link href="" onClick={() => toggleLanguage('en-US')}>
           <ReactCountryFlag className="text-3xl" countryCode="US" svg />
         </Link>
-        <Link href="/pt-BR">
+        <Link href="" onClick={() => toggleLanguage('pt-BR')}>
           <ReactCountryFlag className="text-3xl" countryCode="BR" svg />
         </Link>
       </div>
